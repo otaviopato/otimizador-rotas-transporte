d8eb8f205122ec49371d1d53bbf5bf67f070806a
otaviopato <otaviopato2017@outlook.com>
2024-10-08 16:13:27 -0300

    feat: struct initial

---
+++ src/main/java/com/transporte/rotas/otimizador_de_transporte/OtimizadorDeTransporteApplication.java
@@ -0,0 +1,13 @@
+package com.transporte.rotas.otimizador_de_transporte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtimizadorDeTransporteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtimizadorDeTransporteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/transporte/rotas/otimizador_de_transporte/OtimizadorDeTransporteApplicationTests.java
@@ -0,0 +1,13 @@
+//package com.transporte.rotas.otimizador_de_transporte;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class OtimizadorDeTransporteApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
